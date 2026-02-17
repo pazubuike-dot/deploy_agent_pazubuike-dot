d037b3dfec59bf1d10e8fd76e69be7f8afa63fa7
pazubuike-dot <p.azubuike@alustudent.com>
2026-02-17 20:32:16 +0200

    Summative

--- setup_project.sh
+++ setup_project.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
+
 #Using the user's input to create a directory name
 read -p "Enter your input: " user_input
-mkdir -p "attendance_tracker_$user_input"
-cd "attendance_tracker_$user_input"
+
+signal_trap() {
+    echo "===================================="
+    echo "Script interrupted. archiving ..."
+    archive="attendance_tracker_${user_input}_archive"
+    if [ -d "$dir" ]; then
+    	tar czf "${archive}.tar.gz" "$dir"
+    	rm -rf "$dir"
+    fi
+
+    echo "Directory is archived"
+    exit 1
+}
+trap 'signal_trap' SIGINT
+
+dir="attendance_tracker_$user_input"
+
+sleep 0.7
+mkdir -p "$dir"
 
 #make the attendance.py file
-cat > attendance_checker.py << 'EOF'
-#!/bin/bash
+cat > $dir/attendance_checker.py << 'EOF'
 
 import csv
 import json
@@ -56,12 +73,12 @@ if __name__ == "__main__":
     run_attendance_check()
 EOF
 
-chmod +x attendance_checker.py
+
 
 #making the second file(helpers)
-mkdir -p Helpers
-cat > Helpers/assets.csv << 'EOF'
-#!/bin/bash
+sleep 0.7
+mkdir -p $dir/Helpers
+cat > $dir/Helpers/assets.csv << 'EOF'
 Email,Names,Attendance Count,Absence Count
 alice@example.com,Alice Johnson,14,1
 bob@example.com,Bob Smith,7,8
@@ -69,11 +86,9 @@ charlie@example.com,Charlie Davis,4,11
 diana@example.com,Diana Prince,15,0
 EOF
 
-chmod +x Helpers/assets.csv
 
 #making the config file for json
-cat > Helpers/config.json << 'EOF'
-#!/bin/bash
+cat > $dir/Helpers/config.json << 'EOF'
 {
     "thresholds": {
         "warning": 75,
@@ -84,21 +99,68 @@ cat > Helpers/config.json << 'EOF'
 }
 
 EOF
- chmod +x Helpers/config.json
+
+echo "------------------------------------------------------"
+echo "Update Warning/ Faliure threshold?"
+echo "------------------------------------------------------"
+while true; do
+    read -p "Do you want to update warning threshold? (yes/no): " responseA
+    if [ "$responseA" = "yes" ]; then
+        read -p "Enter new warning threshold: " warn
+        sed -i "s/\"warning\": 75/\"warning\": $warn/" "$dir/Helpers/config.json"
+        sleep 0.4
+        echo "Warning threshold changed to $warn"
+        break
+    elif [ "$responseA" = "no" ]; then
+        echo "continuing ..."
+        break
+    else
+        echo "Invalid response. Please enter yes or no"
+    fi
+done
+
+
+while true; do
+    echo ""
+    read -p "Do you want to update failure threshold? (yes/no): " responseB
+    if [ "$responseB" = "yes" ]; then
+        read -p "Enter new failure threshold: " fail
+        sed -i "s/\"failure\": 50/\"failure\": $fail/" "$dir/Helpers/config.json"
+        sleep 0.4
+	echo ""
+        echo "Failure threshold changed to $fail"
+        break
+    elif [ "$responseB" = "no" ]; then
+	echo "continuing ..."
+        break
+    else
+        echo "Invalid response. Please enter yes or no."
+    fi
+done
+echo "----------------------------------------------------------"
+
 
 #making report file
-mkdir -p reports
-cat > reports/reports.log << 'EOF'
-#!/bin/bash
+sleep 0.7
+mkdir -p $dir/reports
+cat > $dir/reports/reports.log << 'EOF'
+
 --- Attendance Report Run: 2026-02-06 18:10:01.468726 ---
 [2026-02-06 18:10:01.469363] ALERT SENT TO bob@example.com: URGENT: Bob Smith, your attendance is 46.7%. You will fail this class.
 [2026-02-06 18:10:01.469424] ALERT SENT TO charlie@example.com: URGENT: Charlie Davis, your attendance is 26.7%. You will fail this class.
 EOF
 
-chmod +x reports/reports.log
 
-echo "File structure setup successfully"
-
+py_version=$(python3 --version 2>&1)
 
 
-tree
+echo "=============================="
+echo "Python3 Verification:"
+if [ $? -ne 0 ]; then
+    echo "python3: command not found"
+else
+    echo "Success! python3 present"
+fi 
+echo "=============================="
+
+tree ./$dir
